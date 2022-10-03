ec01280e3d0d21bc28df256db4d1c7f61563a8ae
jrasb <okrypczyk@protonmail.com>
2022-10-03 10:16:48 +0200

    added id coordinates minor fixes in css

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
-console.log("file loaded");
-
+// CONST VARIABLES
 const container = document.querySelector("#container");
 
-const generateRandomNumber = function() {
-	return Math.floor(Math.random() * 3);
-}
+//LET VARIABLES
+
+// FUNCTIONS
 
 const generateMap = function() {
-		fetch("/maps/small.json")
-			.then((response) => response.json())
-			.then((json) => {
-				console.log(json.tiles)
-				json.tiles.forEach(
-					(row) => {
-						for (let i = 0; i < row.length; i++){
-							const tileElement = document.createElement("div");
+	let x = -1;
+	let y = 0;
+
+	fetch("/maps/small.json")
+		.then((response) => response.json())
+		.then((json) => {
+			json.tiles.forEach(
+				(row) => {
+					for (let i = 0; i < row.length; i++){
+						const tileElement = document.createElement("div");
+						if (x == 40) {
+							x = 0;
+							y += 1;
+						} else {
+							x += 1;
+						}
+						tileElement.id = x + "." + y;
 						tileElement.className = "tile-class-" + row[i];
 
 						container.appendChild(tileElement);
-						}
 					}
-				)
-			});
+				}
+			)
+		});
 }
 
 generateMap();
